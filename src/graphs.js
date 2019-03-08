0d4805bb7b98c94ddfe49b0f44fe0750e79cd1b1
Jamil Lawrence <jamil.lawrence@gmail.com>
2019-03-07 16:52:40 -0800

    Adds BFS

--- src/graphs.js
+++ src/graphs.js
@@ -55,6 +55,30 @@ class Graph {
     }
     return result;
   }
+
+  breadthFirstTraversal(startingNode) {
+    let result = [];
+    let nodeStack = [];
+    let visitedArr = [];
+
+    nodeStack.push(startingNode);
+    visitedArr[startingNode] = true;
+
+    while (nodeStack.length) {
+      const current = nodeStack.shift();
+      const neighbors = this.adjList[current];
+      result.push(current);
+
+      neighbors.forEach(neighbor => {
+        if (!visitedArr[neighbor]) {
+          nodeStack.push(neighbor);
+          visitedArr[neighbor] = true;
+        }
+      });
+    }
+    return result;
+
+  }
 }
 
 let jamil = new Graph();
@@ -78,8 +102,28 @@ jamil.addEdge(6, 8);
 jamil.addEdge(6, 9);
 jamil.addEdge(99, 4);
 
+let akeem = new Graph();
+akeem.addNode(1);
+akeem.addNode(2);
+akeem.addNode(3);
+akeem.addNode(4);
+akeem.addNode(5);
+akeem.addNode(6);
+akeem.addNode(7);
+akeem.addNode(8);
+akeem.addNode(9);
+akeem.addEdge(1, 2);
+akeem.addEdge(2, 3);
+akeem.addEdge(3, 4);
+akeem.addEdge(4, 5);
+akeem.addEdge(1, 6);
+akeem.addEdge(6, 7);
+akeem.addEdge(7, 8);
+akeem.addEdge(8, 9);
+
 console.log('before delete', jamil);
 console.log('DFS', jamil.depthFirstTraversal(1));
+console.log('BFS', akeem.breadthFirstTraversal(1));
 // jamil.removeNode(3);
 // console.log('after delete', jamil);
 // jamil.addNode(3);
